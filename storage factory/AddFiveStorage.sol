48f9de722d2cbd22fc159d11bf0c462575f8dc19
Deba-07 <dynamicdeba07@gmail.com>
2025-11-08 14:38:06 +0530

    update simple storage and storage factory

---
+++ storage factory/AddFiveStorage.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.30;
+
+import {SimpleStorage} from "./SimpleStorage.sol";
+
+// inheritance
+contract AddFiveStorage is SimpleStorage {
+    function store(uint256 _favoriteNumber) public override {
+        myFavoriteNumber = _favoriteNumber + 5;
+    }
+}
