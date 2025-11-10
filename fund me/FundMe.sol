eb24afe98262aa877df10d88628f046f52eee327
Deba-07 <dynamicdeba07@gmail.com>
2025-11-10 15:21:49 +0530

    feat: implement FundMe contract with Chainlink price feed, funding and withdraw logic

--- fund me/FundMe.sol
+++ fund me/FundMe.sol
@@ -5,15 +5,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.30;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
-    uint256 public minimumUSD = 5;
+    uint256 public minimumUSD = 5e18;
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
     function fund() public payable {
-        require(msg.value > minimumUSD);
+        require(
+            getConversionRate(msg.value) >= minimumUSD,
+            "Didn't send enough ETH"
+        );
+
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function getPrice() public {
+    function getPrice() public view returns (uint256) {
         // address - 0x694AA1769357215DE4FAC081bf1f309aDC325306
         // ABI
 
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+
+        // ETH/USD has 8 decimals, so multiply by 1e10 to get 18 decimals
+        return uint256(price * 1e10);
+    }
+
+    function getConversionRate(
+        uint256 ethAmount
+    ) public view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
     }
 }
