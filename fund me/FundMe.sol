8788001bbf5f20207b61e9ed92d368ef64e708ea
Deba-07 <dynamicdeba07@gmail.com>
2025-11-11 14:39:31 +0530

    update: use price converter library

--- fund me/FundMe.sol
+++ fund me/FundMe.sol
@@ -5,15 +5,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.30;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
     uint256 public minimumUSD = 5e18;
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
     function fund() public payable {
         require(
-            getConversionRate(msg.value) >= minimumUSD,
+            (msg.value.getConversionRate()) >= minimumUSD,
             "Didn't send enough ETH"
         );
 
@@ -21,24 +22,10 @@ contract FundMe {
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function getPrice() public view returns (uint256) {
-        // address - 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        // ABI
-
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(
-            0x694AA1769357215DE4FAC081bf1f309aDC325306
-        );
-        (, int256 price, , , ) = priceFeed.latestRoundData();
-
-        // ETH/USD has 8 decimals, so multiply by 1e10 to get 18 decimals
-        return uint256(price * 1e10);
-    }
-
-    function getConversionRate(
-        uint256 ethAmount
-    ) public view returns (uint256) {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd;
+    function withdraw() public {
+        for(uint256 funderIndex = 0; funderIndex >= funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
     }
 }
